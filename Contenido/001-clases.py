707bbcef81327bfcaef123f74b893ea46d59d656
Victor Saldivia <viktorsaldivia93@gmail.com>
2025-08-27 11:09:53 -0400

    Se agregan nuevos comentarios al archivo

--- Contenido/001-clases.py
+++ Contenido/001-clases.py
@@ -1,10 +1,10 @@
-#GUIA RÁPIDA DE CLASES EN PYTHON
-#Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos
+# GUIA RÁPIDA DE CLASES EN PYTHON
+# Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos
 
 class Persona():
     
-    # Constructort de Clase
-    def __init__(self, nombre, apellido, edad):
+    # Constructor de Clase
+    def __init__(self, nombre, apellido, edad): # este método se ejecuta automáticamente al crear un nuevo objeto
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
@@ -14,7 +14,7 @@ class Persona():
     # apellido = "Torres"
     # edad = 23
     
-    #Métodos (Comportamientos)
+    # Métodos (Comportamientos)
     
     def hablar(self):
         print(f"{self.nombre} esta hablando")
@@ -23,7 +23,7 @@ class Persona():
         print(f"{self.nombre} esta caminando")
         
         
-persona1 = Persona("Cristina", "Torres", 23)
+persona1 = Persona("Cristina", "Torres", 23) # instancia de la clase Persona -> crear un objeto de la clase Persona
 persona2 = Persona("Benjamin", "Gomez", 20)
 
 
@@ -31,6 +31,6 @@ print(f"{persona1.nombre}")
 print(f"{persona1.apellido}")
 print(f"{persona1.edad}")
 
-persona1.hablar()
-persona2.caminar()
+persona1.hablar() # equivalente a Persona.hablar(persona1)
+persona2.caminar() # se puede crear múltiples objetos de la misma clase
         
